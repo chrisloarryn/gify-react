5f5e181cd2a7c54821e97dfe655862444511a1f5
chrisloarryn <chrisloarryn@gmail.com>
2020-07-30 22:50:12 -0400

    added 'docs'

---
+++ docs/precache-manifest.8785cffc47ebede0afb0f3168db0e990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a45f1304122f09de65594c7c616856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30deb57b29ada9c2ba56",
+    "url": "/static/css/main.f6885068.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30deb57b29ada9c2ba56",
+    "url": "/static/js/main.8ebd6f4d.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
